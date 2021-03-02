25c9063f806d047b1e846e39ce919b7194f69df0
mzaini30 <muhzaini30@gmail.com>
2021-03-03 06:13:50 +0800

    upload

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const btoaPro = require('btoa-pro')
-async function mantra(){
-
+async function mantra(perintah, server){
+	const body = new FormData
+	body.append('sql', btoaPro(perintah))
+	let hasil = await fetch(server, {
+		method: 'post',
+		body
+	}).then(x => x.json())
+	hasil = await hasil
+	return hasil
 }
 module.exports = mantra
